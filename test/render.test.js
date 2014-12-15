c2f05d7b3b1ce03698000ea8bbf1737db3344742
Mike Morris <michael.patrick.morris@gmail.com>
2014-12-15 13:42:44 -0500

    compare images

--- test/render.test.js
+++ test/render.test.js
@@ -8,7 +8,6 @@ var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var st = require('st');
-var mkdirp = require('mkdirp');
 
 var dirPath = path.join(path.dirname(require.resolve('mapbox-gl-styles/package.json')), 'styles');
 var server = http.createServer(st({ path: dirPath }));
@@ -19,25 +18,6 @@ function startFixtureServer(callback) {
     });
 }
 
-// These calls are all effectively synchronous though they use a callback.
-test('api', function(t) {
-    new GL(null, function(err) {
-        t.equal(err.toString(), 'Error: uri must be an object');
-    });
-    // @TODO support filepath loading in the future?
-    new GL('gl:///test-style.json', function(err) {
-        t.equal(err.toString(), 'Error: uri must be an object');
-    });
-    new GL({}, function(err) {
-        t.equal(err.toString(), 'Error: uri.style must be a GL style object');
-    });
-    new GL({ style: {} }, function(err, source) {
-        t.ifError(err);
-        t.equal(source instanceof GL, true, 'GL source');
-    });
-    t.end();
-});
-
 function renderTest(name, stylePath) {
     return function(t) {
         var style = require(stylePath);
@@ -46,9 +26,8 @@ function renderTest(name, stylePath) {
             t.deepEqual(source._style, style, 'GL source._style');
             source.getTile(0, 0, 0, function(err, image) {
                 t.ifError(err);
-                var dir = __dirname + '/' + (process.env.UPDATE ? 'expected' : 'actual') + '/';
-                mkdirp(dir);
-                fs.writeFileSync(dir + name + '.png', image);
+                var dir = __dirname + '/fixtures/' + name + '/';
+                fs.writeFileSync(dir + (process.env.UPDATE ? 'expected' : 'actual') + '.png', image);
                 t.end();
             });
         });
