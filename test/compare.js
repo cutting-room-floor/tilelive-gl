33da0fd2eec4bb9793f8d48f80e4fc7b4d023e1b
samanpwbb <samanpwbb@gmail.com>
2015-03-17 14:24:30 -0400

    start rewriting compare.js, update fixtures

--- test/compare.js
+++ test/compare.js
@@ -7,9 +7,10 @@
 var fs = require('fs');
 var path = require('path');
 var spawn = require('child_process').spawn;
-
 var dirPath = path.join(path.resolve('.'), 'test/fixtures');
-var files = fs.readdirSync(dirPath);
+var mkdirp = require('mkdirp');
+var file = require('file');
+var recursive = require('recursive-readdir');
 
 var html =
     '<style>\n' +
@@ -26,47 +27,48 @@ var html =
 var exitCode = 0;
 var failures = 0;
 
+// Get expected, use that to find actuals and diffs
+function processFiles() {
+    recursive(path.join(dirPath, 'expected'), function(err, files) {
+        files.forEach(function(file) {
+            var filename = file.split('expected/').pop();
+
+            function processFileTest() {
+                mkdirp.sync(path.join(dirPath, 'diff'));
+
+                var actual = path.join(dirPath,'actual', filename);
+                var expected = path.join(dirPath,'expected', filename);
+                var diff = path.join(dirPath, 'diff', filename);
+
+                var compare = spawn('compare', ['-metric', 'MAE', actual, expected, diff]);
+                var error = '';
+                compare.stderr.on('data', function(data) {
+                    error += data.toString();
+                });
+                compare.on('exit', function(code, signal) {
+                    // The compare program returns 2 on error otherwise 0 if the images are similar or 1 if they are dissimilar.
+                    if (code == 2) {
+                        writeResult(actual, expected, diff, error.trim(), Infinity);
+                        exitCode = 2;
+                    } else {
+                        var match = error.match(/^\d+(?:\.\d+)?\s+\(([^\)]+)\)\s*$/);
+                        var difference = match ? parseFloat(match[1]) : Infinity;
+                        writeResult(actual, expected, diff, match ? '' : error, difference);
+
+                    }
+                    processFiles();
+                });
+                compare.stdin.end();
+            }
+
+            processFileTest();
+        });
+    });
+};
+
 processFiles();
 
-function processFiles() {
-    if (!files.length) return done();
-
-    var name = files.shift();
-
-    if (name === 'index.html') return processFiles();
-
-    var base = name;
-
-    processFileTest();
-
-    function processFileTest() {
-        var actual = path.join(dirPath, name, 'actual.png');
-        var expected = path.join(dirPath, name, 'expected.png');
-        var diff = path.join(dirPath, name, 'diff.png');
-
-        var compare = spawn('compare', ['-metric', 'MAE', actual, expected, diff]);
-        var error = '';
-        compare.stderr.on('data', function(data) {
-            error += data.toString();
-        });
-        compare.on('exit', function(code, signal) {
-            // The compare program returns 2 on error otherwise 0 if the images are similar or 1 if they are dissimilar.
-            if (code == 2) {
-                writeResult(base, error.trim(), Infinity);
-                exitCode = 2;
-            } else {
-                var match = error.match(/^\d+(?:\.\d+)?\s+\(([^\)]+)\)\s*$/);
-                var difference = match ? parseFloat(match[1]) : Infinity;
-                writeResult(base, match ? '' : error, difference);
-
-            }
-            processFiles();
-        });
-        compare.stdin.end();
-    }
-}
-
-function writeResult(base, error, difference) {
+function writeResult(actual, expected, diff, error, difference) {
     var color = 'green';
     var allowedDifference = 0.001;
     if (difference > allowedDifference) {
@@ -79,10 +81,10 @@ function writeResult(base, error, difference) {
 
     html +=
         '<tr>\n' +
-        '    <td><img src="' + base + '/actual.png" onmouseover="this.src=\'' + base + '/expected.png\'" onmouseout="this.src=\'' + base + '/actual.png\'"></td>\n' +
-        '    <td><img src="' + base + '/expected.png" onmouseover="this.src=\'' + base + '/diff.png\'" onmouseout="this.src=\'' + base + '/expected.png\'"></td>\n' +
+        '    <td><img src="' + actual + '" onmouseover="this.src=\'' + expected + '\'" onmouseout="this.src=\'' + actual + '\'"></td>\n' +
+        '    <td><img src="' + expected + '" onmouseover="this.src=\'' + diff + '\'" onmouseout="this.src=\'' + expected + '\'"></td>\n' +
         '    <td>\n' +
-        '        <h2 style="text-align:center; background:' + color + '"><a href="' + base + '/style.json">' + base + '</a></h2>\n' +
+        '        <h2 style="text-align:center; background:' + color + '">' + actual + '</a></h2>\n' +
         (error ? '        <p>' + error + '</p>\n' : '') +
         '        <ul>\n' +
         '            <li>diff: <strong>' + difference + '</strong></li>\n' +
