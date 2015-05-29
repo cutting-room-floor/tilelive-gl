a180469b93c5f246b62594e27e67a088d16b65ab
Mike Morris <michael.patrick.morris@gmail.com>
2015-05-29 17:16:54 -0400

    add getStatic method to pass options directly to node-mbgl

--- index.js
+++ index.js
@@ -80,7 +80,26 @@ GL.prototype.getTile = function(z, x, y, callback) {
         var concatStream = concat(function(buffer) {
             return callback(null, buffer, { 'Content-Type': 'image/png' });
         });
-         
+
+        png.pack().pipe(concatStream);
+    });
+};
+
+GL.prototype.getStatic = function(options, callback) {
+    this._map.render(options, function(err, data) {
+        if (err) return callback(err);
+
+        var png = new PNG({
+            width: data.width,
+            height: data.height
+        });
+
+        png.data = data.pixels;
+
+        var concatStream = concat(function(buffer) {
+            return callback(null, buffer, { 'Content-Type': 'image/png' });
+        });
+
         png.pack().pipe(concatStream);
     });
 };
